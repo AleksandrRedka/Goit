3a605d21c3a474feda152fd8f64a49063f0d3b2d
Aleksandr Redka <Aleksandr Redka>
2018-12-13 14:14:14 +0200

    js-module03

--- JS/module-03/js/index.js
+++ JS/module-03/js/index.js
@@ -1,30 +1,37 @@
-'use strict'
+"use strict";
+
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
-let allLogins=logins
+
 const isLoginValid = function(login) {
-  let logLength=login.length
-  return 4<=logLength && logLength<=16
-  // код
-}
-const isLoginUnique = function(allLogins, login) {
-  return allLogins.includes(login);
-  // код;
-}
-const addLogin = function(login){
-  if(isLoginValid(login)){
-    if(!isLoginUnique(allLogins,login)){
-      allLogins.push(login)
-      return'Логин успешно добавлен!'
-    }else{
-      return'Такой логин уже используется!'
-    }
-  }else{
-    return 'Ошибка! Логин должен быть от 4 до 16 символов'
-  }
-  // код
+  let loginLeng = login.length;
+  return 4 <= loginLeng && loginLeng <= 16;
 };
+
+
+const isLoginUnique = function(logins, login) {
+  return logins.includes(login);
+};
+
+
+const addLogin = function(login) {
+  if (!isLoginValid(login)) {
+    console.log("Ошибка! Логин должен быть от 4 до 16 символов");
+    return;
+  } else {
+    if (!isLoginUnique(logins, login)) {
+      console.log("Такой логин уже используется!");
+      return;
+    } else {
+      logins.push(login);
+      console.log("Логин успешно добавлен!");
+      return;
+    }
+  }
+};
+
+
 // Вызовы функции для проверки
-console.log(addLogin('Ajax')); // 'Логин успешно добавлен!'
-console.log(addLogin('robotGoogles')); // 'Такой логин уже используется!'
-console.log(addLogin('Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
-console.log(addLogin('jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin("Ajax"); // 'Логин успешно добавлен!'
+addLogin("robotGoogles"); // 'Такой логин уже используется!'
+addLogin("Zod"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+addLogin("jqueryisextremelyfast"); // 'Ошибка! Логин должен быть от 4 до 16 символов'
