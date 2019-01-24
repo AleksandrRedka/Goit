5f8ee60503b7fab6f2958c97948625550dd0ba0b
Aleksandr Redka <Aleksandr Redka>
2019-01-24 12:44:57 +0200

    test_task_smm1.2

---
+++ portfolio/TestTask2/assets/js/index.js
@@ -0,0 +1,19 @@
+$('.slick_slader').slick();
+
+
+// Скрытый текст Кнопка читать
+
+$('.slick__text').readmore({
+    maxHeight: 100,
+    heightMargin: 50,
+    moreLink: '<a href="#" class="slick__readmore">Читати...</a>',
+    lessLink: '<a href="#" class="slick__readmore">Сховати</a>'
+});
+
+// Меню на мобильной версии
+$(".mobil__nav__tach").click(function(){
+    $(".mobil__nav__header__box").toggleClass("mobil__nav__bg__white")
+    $(".mobil__nav__button").toggleClass("mobil__nav__close");
+    $(".mobil__nav__button").toggleClass("mobil__nav__open");
+    $(".mobil__menu").toggleClass("open")
+})
