cd938d28696bfc944d1656ea23839cd3087b40f7
Aleksandr Redka <Aleksandr Redka>
2019-01-25 13:41:39 +0200

    test_task_smm1.3

--- portfolio/TestTask2/assets/js/index.js
+++ portfolio/TestTask2/assets/js/index.js
@@ -16,4 +16,17 @@ $(".mobil__nav__tach").click(function(){
     $(".mobil__nav__button").toggleClass("mobil__nav__close");
     $(".mobil__nav__button").toggleClass("mobil__nav__open");
     $(".mobil__menu").toggleClass("open")
+});
+
+
+//  Поворот меню при скроле
+
+$(document).on("scroll", function(){
+    let lengthScroll = $(document).scrollTop();
+    console.log(lengthScroll)
+    if (lengthScroll>200){
+        $(".nav__bg").addClass("nav__bg__scroll")
+    }else{
+        $(".nav__bg").removeClass("nav__bg__scroll")
+    }
 })
